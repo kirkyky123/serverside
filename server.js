5d7458bbc34908faefbd2a3d2456b3810b9c4e3c
kirkyky123 <131261278+kirkyky123@users.noreply.github.com>
2024-03-26 11:12:29 -0400

    yes

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 
-app.use((req, res, next) => {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
